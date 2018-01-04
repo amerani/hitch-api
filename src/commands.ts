c0bde10d0523cfcf60f9880f2d7397da4f8d5fa3
alek <alek.merani+hex10@gmail.com>
2018-01-03 23:24:49 -0600

    Refactor create account command to use repository

--- src/commands.ts
+++ src/commands.ts
@@ -7,15 +7,13 @@ export async function createAccountAsync(
     {firstName, lastName, email, passwordHash})
     : Promise<User> {
 
-        const user = new User();
-        user.firstName = firstName;
-        user.lastName = lastName;
-        const userAccount = new UserAccount();
-        userAccount.email = email;
-        userAccount.passwordHash = passwordHash;
-        user.userAccount = userAccount;
-
-        return getManager().save(user);
+        const userRepo = getRepository(User);
+        const user = userRepo
+            .merge(new User(), {
+                firstName, lastName, 
+                userAccount: { email, passwordHash}
+            });
+        return userRepo.save(user);
 }
 
 export async function createReservationAsync(
