113ed6b04e354238748fbd6892102742b5232857
alek <alek.merani+hex10@gmail.com>
2018-01-07 11:17:41 -0600

    Test fetching open reservations of a trip

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import "reflect-metadata";
-import {createConnection, getConnection} from "typeorm";
-import {createRoundTripListing} from "./examples/createRoundTripListing";
+import {createConnection, getConnection, getRepository} from "typeorm";
+import {createRoundTripListingAsync} from "./examples/createRoundTripListing";
+import { createAccountAsync } from "./commands";
+import { Transport } from "./entity/Transport";
+import { Leg } from "./entity/Leg";
+import { Reservation } from "./entity/Reservation";
+
+async function getOpenReservations(leg: Leg): Promise<Reservation[]> {
+    const transport = await getRepository(Transport).findOneById(leg.transport.id);
+    return transport.reservations.filter(res => res.reservedBy == null);
+}
 
 createConnection().then(async connection => {
 
-    await createRoundTripListing();
+    const user = await createAccountAsync({
+        firstName: "alek",
+        lastName: "merani",
+        email: `${Date.now()}@g.co`,
+        passwordHash: "password"
+    });
+
+    const trip = await createRoundTripListingAsync(user);
+
+    const rezz = await Promise.all(trip.legs.map(getOpenReservations));
+
+    console.log(rezz);
 
     connection.close();
 
